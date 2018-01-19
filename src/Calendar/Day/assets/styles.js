25f03e6c59479b98a2df775c3958878057e49233
Anish Duwal <duwalanise@gmail.com>
2018-01-19 11:59:20 +0545

    add daily calendar

---
+++ src/Calendar/Day/assets/styles.js
@@ -0,0 +1,82 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const {width} = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection    : 'column',
+    flex             : 1,
+    width,
+    backgroundColor  : '#fff',
+    paddingHorizontal: 10,
+    paddingVertical  : 5 
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  hours: {
+    width         : 75,
+    justifyContent: 'center',
+    alignItems    : 'center',
+    height        : 50
+  },
+  events: {
+    justifyContent: 'center',
+    alignItems    : 'stretch',
+    height        : 50,
+    flex          : 1,
+    padding       : 2
+  },
+  line: {
+    borderWidth: 0.5,
+    borderColor: '#ccc'
+  },
+  fixedHeader: {
+    width          : 75,
+    alignItems     : 'center',
+    backgroundColor: '#fff',
+    marginBottom   : -50,
+    marginTop      : -5,
+    paddingTop     : 5,
+    zIndex         : 1
+  },
+  day: {
+    color: '#1F84DD'
+  },
+  date: {
+    fontSize: 30,
+    color   : '#1F84DD'
+  },
+  currentTime: {
+    alignItems   : 'center',
+    flexDirection: 'row',
+  },
+  currentTimeCircle: {
+    width          : 8,
+    height         : 8,
+    borderRadius   : 4,
+    backgroundColor: '#ff0000'
+  },
+  currentTimeLine: {
+    flex       : 1,
+    borderWidth: 0.5,
+    borderColor: '#ff0000',
+  },
+  currentEvent: {
+    borderRadius   : 3,
+    backgroundColor: '#7D1FDD',
+    height         : 50,
+    paddingTop     : 5,
+    paddingLeft    : 15,
+    flex           : 1,
+    shadowColor    : '#ccc',
+    shadowOpacity  : 0.8,
+    shadowRadius   : 2,
+    shadowOffset   : {
+      height: 1,
+      width : 0
+    }
+  }
+});
+
+export default styles;
---
+++ src/Calendar/Day/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ScrollView, View, Text } from 'react-native';
+import styles from './assets/styles';
+import { generateDayArray, groupEvents } from 'src/generics/helpers/calendar';
+
+const defaultHeader = currentDate =>
+  <View style={styles.fixedHeader}>
+    <Text style={styles.day}>{currentDate.format('ddd')}</Text>
+    <Text style={styles.date}>{currentDate.date()}</Text>
+  </View>;
+
+const defaultBody = currentDate => <View style={styles.line} />
+
+const DailyCalendar = props => {
+  const { headerCell, bodyCell, RNCurrentDate, title, bodyStyle, headerStyle } = props;
+  const previousDay = RNCurrentDate.clone().subtract(1, 'days');
+  return [1,2,3].map(count => {
+    const currentDate = previousDay.clone().add(count, 'days');
+    return (
+      <View style={styles.container}>
+        {headerCell(currentDate)}
+        <ScrollView style={{flex: 1}}>
+          {
+            generateDayArray(currentDate).map((hours, idx) => {
+              return (
+                <View style={styles.row}>
+                  <View style={styles.hours}><Text>{hours.format('h A')}</Text></View>
+                  <View style={styles.events}>
+                    {bodyCell(hours)}
+                  </View>
+                </View>
+              );
+            }
+            )
+          }
+        </ScrollView>
+      </View>
+    )
+  });
+}
+
+export default DailyCalendar;
+
+DailyCalendar.propTypes = {
+  headerCell: PropTypes.element,
+  title: PropTypes.element,
+  bodyCell: PropTypes.element,
+  RNCurrentDate: PropTypes.object,
+  bodyStyle: PropTypes.object,
+  headerStyle: PropTypes.object
+};
+
+DailyCalendar.defaultProps = {
+  headerCell: defaultHeader,
+  title: null,
+  bodyCell: defaultBody,
+  bodyStyle: {},
+  headerStyle: {}
+}
